39bf539d23efdd5cc982a69856857598bb39f9a4
Julien Pinsonneau <jpinsonn@redhat.com>
2022-08-19 12:09:43 +0200

    react fixes

--- rollup.config.js
+++ rollup.config.js
@@ -7,6 +7,7 @@ const packageJson = require("./package.json");
 
 export default [
   {
+    external: ["react", "react-dom"],
     input: "src/index.ts",
     output: [
       {
@@ -27,7 +28,7 @@ export default [
     ],
   },
   {
-    input: "dist/esm/index.d.ts",
+    input: "dist/esm/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
   },
